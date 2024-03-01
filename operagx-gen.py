3bfbc84db6f60f2dc8dcbdd7a8a635ea510195e4
Kroko1337 <krokoonair@gmail.com>
2024-03-01 15:00:52 +0100

    Fixed their autism fix

--- operagx-gen.py
+++ operagx-gen.py
@@ -4,11 +4,12 @@ import time
 
 
 def generate_nitro_gift():
-    url = 'https://api.discord.gx.games/v1/direct-fulfillment'
+    url = 'https://discord.opr.gg/v2/direct-fulfillment'
     headers = {
-        'authority': 'api.discord.gx.games',
+        'authority': 'discord.opr.gg',
         'accept': '*/*',
         'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI3NjhlOTcxZi0zNzcyLTRmYjMtOTlhNC04OGQ5MDMzYjBkNzEiLCJleHAiOjE3MDkzMDE3NDJ9.tfimKKamOYLzPxevebOtxJn3kZu-FlYK2_jS8dUOQozDf326QIHDN8DVq0qejrfYHdgQZkLHbrSeCdDV31OZgquR03cM0Fp93lBPoJEfdFnbh4BkCCLuHUyddf0EoCLy5uM1HpufIdNWS9IjDseuOl85OMWRocvZXIzMGUukNgwF6d1MZsE-8NThkd8uK5wt0SJdganvVDToPNaMK277hlaZkeFwtrJuKmvrp6M48ECLo-GaT9D9fwLg2N2pf7MZSYwMwKQj1pt4UuTZ1xrm8olIBS-ZhnfsAU7cH0F1ZrxrEnCk8UC0YfZXx96bR0Dlt5rubxFGf5DjXKx2AvlloyoAFKRsxXH71jvmvF9EjlsRj3A12H4x7UMKm42O9JiyI2nhc87jiUXYz6OqUknDwW8czCl5Jdd8nyGA34mtTDGluUhKM14Pcp4whuZ9a-zMiqpM1236_fpN1sG7fzlIT9zxACRkTfVShgf2u0m6IArFa8h1nJ5xG0pUF3-IbuCwUuVSFawCpsizcAU2XuaofMGfKWh2AjZJ1oM5shnz9ftZd5ulHl5XlcdJcOjRACsJCfDM3BRxdk3SJ16sgQtEdqBFA-vKhx6jZRl20RZRiocJd_1pYNHVQtj5zEQojVXxzRMrvRJkaGHBbITpR7GivDL4VPT5wP_YwT0LdGEw3ck',
         'content-type': 'application/json',
         'origin': 'https://www.opera.com',
         'referer': 'https://www.opera.com/',
